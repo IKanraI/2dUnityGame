55cfd021fe1563824bf16e8d295e3f7ce70040c8
IKanraI <joelmyrday@hotmail.com>
2021-08-12 04:13:51 -0400

    Fine tuned the jump animation and death animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour {
     private Collider2D col;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
-    private Animation animation;
 
     private void Awake() {
         playerControls = new PlayerActionControls();
@@ -21,19 +20,10 @@ public class PlayerController : MonoBehaviour {
         col = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animation = GetComponent<Animation>();
-    }
-
-    private void OnEnable() {
-        playerControls.Enable();
-    }
-    
-    private void OnDisable() {
-        playerControls.Disable();
     }
     
     void Start() {
-        playerControls.Land.Jump.performed += _ => Jump();
+        playerControls.Land.Jump.started += _ => Jump();
     }
 
     private bool IsGrounded() {
@@ -44,11 +34,14 @@ public class PlayerController : MonoBehaviour {
         Vector2 bottomRightPoint = transform.position;
         bottomRightPoint.x += col.bounds.extents.x;
         bottomRightPoint.y -= col.bounds.extents.y;
+        
         return Physics2D.OverlapArea(topLeftPoint, bottomRightPoint, ground);
     }
 
     // Update is called once per frame
     void Update() {
+        if (animator.GetBool("isJumpingBool") == true)
+            StartCoroutine(WaitForJumpEnd());
         Move();
     }
 
@@ -67,12 +60,12 @@ public class PlayerController : MonoBehaviour {
         //read movement value
         float input = playerControls.Land.Move.ReadValue<float>();
         float inputCrouch = playerControls.Land.Crouch.ReadValue<float>();
+        bool playerDirectionForward = true;
 
         //move the player
         Vector3 currPos = transform.position;
         currPos.x += input * speed * Time.deltaTime;
         transform.position = currPos;
-
         
         //animation
         Crouch(inputCrouch);
@@ -94,31 +87,60 @@ public class PlayerController : MonoBehaviour {
         if (input == 0 && inputCrouch == 0)
             animator.SetBool("isIdle", true);
 
-        //sprite flip
+        //sprite flip //idk do some shit with keeping the direction in a variable and fucking flip it
         if (input == -1) {
-            animator.SetTrigger("isTurning");
             spriteRenderer.flipX = true;
+            if (!playerDirectionForward) {
+                animator.SetTrigger("isTurning");
+                Debug.Log("meow");
+            }
+            playerDirectionForward = false;
         }
         else if (input == 1) {
             //animator.SetTrigger("isTurning");
             spriteRenderer.flipX = false;
+            playerDirectionForward = true;
         }
     }
 
     private void Jump() {
         if (IsGrounded()) {
+            Debug.Log("Jump?");
             body.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
-
-            animator.SetTrigger("isJumping");
-        
+            animator.SetBool("isJumpingBool", true);
         }
     }
-   private void OnCollisionEnter2D(Collision2D other) {
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        //Collision with an enemy tag that causes a death
        if (other.gameObject.tag == "Enemy") {
            animator.SetTrigger("isDead");
-           
-          // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-       }
-   }
+           StartCoroutine(WaitAnimation());
+        }
+    }
 
+   // Custom waiting animations on things that rely on time
+
+    private IEnumerator WaitAnimation() {
+        OnDisable();
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator WaitForJumpEnd() {
+        yield return new WaitForSeconds(0.05f);
+        if (IsGrounded())
+            animator.SetBool("isJumpingBool", false);
+        
+    }
+
+    //Needed for sprite animation to work
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+    
+    private void OnDisable() {
+        playerControls.Disable();
+    }
 }
